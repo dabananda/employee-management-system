94c0d6ee0d801e512457e5e9b9b4ed7030a0581e
dabananda <imdmitra@gmail.com>
2024-10-05 14:31:18 +0600

    Implemented Create Department Functionality

--- ems-frontend/src/components/DepartmentComponent.jsx
+++ ems-frontend/src/components/DepartmentComponent.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
+import { createDepartment } from '../services/DepartmentService';
+import { useNavigate } from 'react-router-dom';
 
 const DepartmentComponent = () => {
   const [departmentName, setDepartmentName] = useState('');
   const [departmentDescription, setDepartmentDescription] = useState('');
 
+  const navigator = useNavigate();
+
   function saveDepartment(e) {
     e.preventDefault();
     const department = { departmentName, departmentDescription };
-    console.log(department);
+    createDepartment(department)
+      .then((response) => {
+        navigator('/departments')
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   return (
